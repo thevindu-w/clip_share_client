1d5682665d6f7346db1f76bff33481e1f7cd7da2
thevindu-w <thevinduwijesekera99@gmail.com>
2025-01-29 23:39:35 +0530

    Implement IPv6 multicast scanning

--- app/src/main/java/com/tw/clipshare/ServerFinder.java
+++ app/src/main/java/com/tw/clipshare/ServerFinder.java
@@ -26,6 +26,7 @@ package com.tw.clipshare;
 
 import java.io.IOException;
 import java.net.*;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -33,8 +34,11 @@ import java.util.concurrent.TimeUnit;
 
 class ServerFinder implements Runnable {
 
+  private static final byte[] SCAN_MSG = "in".getBytes(StandardCharsets.UTF_8);
   private static final HashMap<String, InetAddress> serverAddresses = new HashMap<>(2);
+  private static final Set<InetAddress> myAddresses = new HashSet<>(2);
   private static ExecutorService executorStatic;
+  private static InetAddress multicastGroup;
   private final NetworkInterface netIF;
   private final Thread parent;
   private final int port;
@@ -51,8 +55,10 @@ class ServerFinder implements Runnable {
     try {
       synchronized (serverAddresses) {
         serverAddresses.clear();
+        myAddresses.clear();
       }
       if (executorStatic != null) executorStatic.shutdownNow();
+      if (multicastGroup == null) multicastGroup = Inet6Address.getByName("ff05::4567");
       Enumeration<NetworkInterface> netIFEnum = NetworkInterface.getNetworkInterfaces();
       Object[] netIFList = Collections.list(netIFEnum).toArray();
       executorStatic = Executors.newFixedThreadPool(netIFList.length);
@@ -70,7 +76,7 @@ class ServerFinder implements Runnable {
         }
         try {
           //noinspection ResultOfMethodCallIgnored
-          executor.awaitTermination(500, TimeUnit.MILLISECONDS);
+          executor.awaitTermination(600, TimeUnit.MILLISECONDS);
         } catch (InterruptedException ignored) {
           break;
         }
@@ -82,25 +88,43 @@ class ServerFinder implements Runnable {
     }
     List<InetAddress> addresses;
     synchronized (serverAddresses) {
-      addresses = new ArrayList<>(serverAddresses.values());
+      addresses = new ArrayList<>(serverAddresses.size());
+      for (InetAddress address : serverAddresses.values()) {
+        if (address instanceof Inet4Address) {
+          addresses.add(address);
+          continue;
+        }
+        boolean isOther = true;
+        for (InetAddress myAddress : myAddresses) {
+          if (Arrays.equals(myAddress.getAddress(), address.getAddress())) {
+            isOther = false;
+            break;
+          }
+        }
+        if (isOther) addresses.add(address);
+      }
+      serverAddresses.clear();
+      myAddresses.clear();
     }
+    //noinspection ResultOfMethodCallIgnored
+    Thread.interrupted();
     return addresses;
   }
 
-  private void scanUDP(Inet4Address broadcastAddress, Inet4Address myAddress) {
+  private void scanBroadcast(Inet4Address broadcastAddress, Inet4Address myAddress) {
     new Thread(
             () -> {
               try {
                 DatagramSocket socket = new DatagramSocket();
-                byte[] buf = "in".getBytes();
-                DatagramPacket pkt = new DatagramPacket(buf, buf.length, broadcastAddress, portUDP);
+                DatagramPacket pkt =
+                    new DatagramPacket(SCAN_MSG, SCAN_MSG.length, broadcastAddress, portUDP);
                 socket.send(pkt);
-                buf = new byte[256];
+                byte[] buf = new byte[256];
                 pkt = new DatagramPacket(buf, buf.length);
                 int timeout = 1000;
                 while (true) {
                   socket.setSoTimeout(timeout);
-                  timeout = 200;
+                  timeout = 250;
                   try {
                     socket.receive(pkt);
                   } catch (SocketTimeoutException ignored) {
@@ -108,6 +132,48 @@ class ServerFinder implements Runnable {
                   }
                   InetAddress serverAddress = pkt.getAddress();
                   if (myAddress.equals(serverAddress)) continue;
+                  String received = new String(pkt.getData()).replace("\0", "");
+                  if ("clip_share".equals(received)) {
+                    String addressStr = serverAddress.getHostAddress();
+                    if (addressStr != null) {
+                      addressStr = addressStr.intern();
+                      synchronized (serverAddresses) {
+                        serverAddresses.put(addressStr, serverAddress);
+                      }
+                    }
+                  }
+                }
+                if (!serverAddresses.isEmpty()) parent.interrupt();
+                socket.close();
+              } catch (IOException | RuntimeException ignored) {
+              }
+            })
+        .start();
+  }
+
+  private void scanMulticast(Inet6Address ifAddress) {
+    new Thread(
+            () -> {
+              try {
+                MulticastSocket socket = new MulticastSocket();
+                socket.setInterface(ifAddress);
+                socket.setTimeToLive(4);
+                DatagramPacket pkt =
+                    new DatagramPacket(SCAN_MSG, SCAN_MSG.length, multicastGroup, portUDP);
+                socket.send(pkt);
+                byte[] buf = new byte[256];
+                pkt = new DatagramPacket(buf, buf.length);
+                int timeout = 1000;
+                while (true) {
+                  socket.setSoTimeout(timeout);
+                  timeout = 250;
+                  try {
+                    socket.receive(pkt);
+                  } catch (SocketTimeoutException ignored) {
+                    break;
+                  }
+                  InetAddress serverAddress = pkt.getAddress();
+                  if (ifAddress.equals(serverAddress)) continue;
                   String received = new String(pkt.getData()).replace("\0", "");
                   if ("clip_share".equals(received)) {
                     String addressStr = serverAddress.getHostAddress();
@@ -135,15 +201,21 @@ class ServerFinder implements Runnable {
       }
       List<InterfaceAddress> addresses = netIF.getInterfaceAddresses();
       for (InterfaceAddress intAddress : addresses) {
+        InetAddress address = intAddress.getAddress();
+        if (address instanceof Inet6Address) {
+          myAddresses.add(address);
+        }
+      }
+      for (InterfaceAddress intAddress : addresses) {
         try {
           InetAddress address = intAddress.getAddress();
           if (address instanceof Inet4Address) {
             InetAddress broadcastAddress = intAddress.getBroadcast();
             if (broadcastAddress instanceof Inet4Address) {
-              scanUDP((Inet4Address) broadcastAddress, (Inet4Address) address);
+              scanBroadcast((Inet4Address) broadcastAddress, (Inet4Address) address);
             }
             short subLen = intAddress.getNetworkPrefixLength();
-            if (subLen < 22) subLen = 23;
+            if (subLen <= 22) subLen = 23;
             SubnetScanner subnetScanner = new SubnetScanner(address, port, subLen);
             InetAddress server = subnetScanner.scan(subLen >= 24 ? 32 : 64);
             if (server != null) {
@@ -156,6 +228,8 @@ class ServerFinder implements Runnable {
               }
               break;
             }
+          } else if (address instanceof Inet6Address) {
+            scanMulticast((Inet6Address) address);
           }
         } catch (RuntimeException ignored) {
         }
